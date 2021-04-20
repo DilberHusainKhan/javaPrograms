2d79c8f3d9eba70f5e913f67fc2e5a1aab31084e
Dilber Husain Khan <dilberjmi@gmail.com>
2021-04-20 11:00:59 +0530

    interface program

---
+++ clg_Practice/clg_2/manageinterface.java
@@ -0,0 +1,26 @@
+interface mobile{
+    int voltage(int v);
+    String modeln(String m);
+}
+interface usb{
+    String usbf(String s);
+}
+
+class user implements mobile,usb{
+    public int voltage(int v){
+        return v;
+    }
+    public String modeln(String m){
+        return m;
+    }
+    public String usbf(String s){
+        return s;
+    }
+}
+class manageinterface{
+    public static void main(String[] args) {
+        user obj =new user();
+        System.out.println(obj.voltage(12));
+        System.out.println(obj.modeln("REALME XT"));
+    }
+}

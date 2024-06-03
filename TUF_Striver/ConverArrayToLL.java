9224873096b71e97eebc34651725c7019c030eea
dilberhusainkhan <dilberjmi@gmail.com>
2024-06-03 17:58:49 +0530

    Striver 1 question Array to Linklist

---
+++ TUF_Striver/ConverArrayToLL.java
@@ -0,0 +1,44 @@
+class Node{
+    int data;
+    Node next;
+    Node(int data, Node next){
+        this.data = data;
+        this.next= next;
+    }
+    Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+/**
+ * ConverArrayToLL
+ */
+public class ConverArrayToLL {
+
+    public static Node convertArr2LL(int[] arr){
+        Node head = new Node(arr[0]);
+        Node mover = head;
+        for(int i=1;i<arr.length;i++){
+            Node newNode =new Node(arr[i]);
+            mover.next = newNode;
+            mover=mover.next;
+        }
+        return head;
+    }
+
+    public static void printAllNode(Node head){
+        Node temp = head;
+        System.out.print("Data");
+        do{
+            System.out.print("-->"+temp.data);
+            temp=temp.next;
+        }while (temp.next!=null);
+        System.out.print("-->"+temp.data);
+    }
+    public static void main(String[] args) {
+        int [] arr={1,2,3,4,5,6};
+        Node head= convertArr2LL(arr);
+        System.out.println("Head -->"+head.data);
+        printAllNode(head);
+    }
+}
